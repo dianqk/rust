c12ff2aaaf2319daa74b5286a6ca271149439a43
dianqk <dianqk@dianqk.net>
2025-03-29 20:04:40 +0800

    Link object files that use `#[used]`

--- compiler/rustc_codegen_ssa/src/base.rs
+++ compiler/rustc_codegen_ssa/src/base.rs
@@ -8,7 +8,7 @@ use rustc_abi::FIRST_VARIANT;
 use rustc_ast as ast;
 use rustc_ast::expand::allocator::{ALLOCATOR_METHODS, AllocatorKind, global_fn_name};
 use rustc_attr_parsing::OptimizeAttr;
-use rustc_data_structures::fx::{FxHashMap, FxIndexSet};
+use rustc_data_structures::fx::{FxHashMap, FxIndexMap, FxIndexSet};
 use rustc_data_structures::profiling::{get_resident_set_size, print_time_passes_entry};
 use rustc_data_structures::sync::par_map;
 use rustc_data_structures::unord::UnordMap;
@@ -875,8 +875,21 @@ impl CrateInfo {
             .iter()
             .map(|&c| (c, crate::back::linker::exported_symbols(tcx, c)))
             .collect();
-        let linked_symbols =
-            crate_types.iter().map(|&c| (c, crate::back::linker::linked_symbols(tcx, c))).collect();
+        let mut linked_symbols: FxIndexMap<CrateType, Vec<(String, SymbolExportKind)>> =
+            crate_types.iter().map(|&c| (c, Vec::new())).collect();
+        let linked_objects = crate_types
+            .iter()
+            .map(|&c| {
+                (
+                    c,
+                    crate::back::linker::linked_objects(
+                        tcx,
+                        c,
+                        linked_symbols.get_mut(&c).unwrap(),
+                    ),
+                )
+            })
+            .collect();
         let local_crate_name = tcx.crate_name(LOCAL_CRATE);
         let crate_attrs = tcx.hir_attrs(rustc_hir::CRATE_HIR_ID);
         let subsystem =
@@ -922,6 +935,7 @@ impl CrateInfo {
             crate_types,
             exported_symbols,
             linked_symbols,
+            linked_objects,
             local_crate_name,
             compiler_builtins,
             profiler_runtime: None,
