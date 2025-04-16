e3634d073b7a04a9b1a81d8449e8ecd73602e17c
dianqk <dianqk@dianqk.net>
2025-04-16 19:01:20 +0800

    wip

--- compiler/rustc_mir_transform/src/dead_store_elimination.rs
+++ compiler/rustc_mir_transform/src/dead_store_elimination.rs
@@ -25,109 +25,6 @@ use rustc_mir_dataflow::impls::{
 use crate::ssa::SsaLocals;
 use crate::util::is_within_packed;
 
-/// Performs the optimization on the body
-///
-/// The `borrowed` set must be a `DenseBitSet` of all the locals that are ever borrowed in this
-/// body. It can be generated via the [`borrowed_locals`] function.
-fn eliminate<'tcx>(tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>) {
-    let borrowed_locals = borrowed_locals(body);
-
-    // If the user requests complete debuginfo, mark the locals that appear in it as live, so
-    // we don't remove assignments to them.
-    let mut always_live = debuginfo_non_ssa_locals(tcx, body);
-    always_live.union(&borrowed_locals);
-
-    let mut live = MaybeTransitiveLiveLocals::new(&always_live)
-        .iterate_to_fixpoint(tcx, body, None)
-        .into_results_cursor(body);
-
-    // For blocks with a call terminator, if an argument copy can be turned into a move,
-    // record it as (block, argument index).
-    let mut call_operands_to_move = Vec::new();
-    let mut patch = Vec::new();
-
-    for (bb, bb_data) in traversal::preorder(body) {
-        if let TerminatorKind::Call { ref args, .. } = bb_data.terminator().kind {
-            let loc = Location { block: bb, statement_index: bb_data.statements.len() };
-
-            // Position ourselves between the evaluation of `args` and the write to `destination`.
-            live.seek_to_block_end(bb);
-            let mut state = live.get().clone();
-
-            for (index, arg) in args.iter().map(|a| &a.node).enumerate().rev() {
-                if let Operand::Copy(place) = *arg
-                    && !place.is_indirect()
-                    // Do not skip the transformation if the local is in debuginfo, as we do
-                    // not really lose any information for this purpose.
-                    && !borrowed_locals.contains(place.local)
-                    && !state.contains(place.local)
-                    // If `place` is a projection of a disaligned field in a packed ADT,
-                    // the move may be codegened as a pointer to that field.
-                    // Using that disaligned pointer may trigger UB in the callee,
-                    // so do nothing.
-                    && is_within_packed(tcx, body, place).is_none()
-                {
-                    call_operands_to_move.push((bb, index));
-                }
-
-                // Account that `arg` is read from, so we don't promote another argument to a move.
-                LivenessTransferFunction(&mut state).visit_operand(arg, loc);
-            }
-        }
-
-        for (statement_index, statement) in bb_data.statements.iter().enumerate().rev() {
-            let loc = Location { block: bb, statement_index };
-            if let StatementKind::Assign(assign) = &statement.kind {
-                if !assign.1.is_safe_to_remove() {
-                    continue;
-                }
-            }
-            match &statement.kind {
-                StatementKind::Assign(box (place, _))
-                | StatementKind::SetDiscriminant { place: box place, .. }
-                | StatementKind::Deinit(box place) => {
-                    if !place.is_indirect() && !always_live.contains(place.local) {
-                        live.seek_before_primary_effect(loc);
-                        if !live.get().contains(place.local) {
-                            patch.push(loc);
-                        }
-                    }
-                }
-                StatementKind::Retag(_, _)
-                | StatementKind::StorageLive(_)
-                | StatementKind::StorageDead(_)
-                | StatementKind::Coverage(_)
-                | StatementKind::Intrinsic(_)
-                | StatementKind::ConstEvalCounter
-                | StatementKind::PlaceMention(_)
-                | StatementKind::BackwardIncompatibleDropHint { .. }
-                | StatementKind::Nop => {}
-
-                StatementKind::FakeRead(_) | StatementKind::AscribeUserType(_, _) => {
-                    bug!("{:?} not found in this MIR phase!", statement.kind)
-                }
-            }
-        }
-    }
-
-    if patch.is_empty() && call_operands_to_move.is_empty() {
-        return;
-    }
-
-    let bbs = body.basic_blocks.as_mut_preserves_cfg();
-    for Location { block, statement_index } in patch {
-        bbs[block].statements[statement_index].make_nop();
-    }
-    for (block, argument_index) in call_operands_to_move {
-        let TerminatorKind::Call { ref mut args, .. } = bbs[block].terminator_mut().kind else {
-            bug!()
-        };
-        let arg = &mut args[argument_index].node;
-        let Operand::Copy(place) = *arg else { bug!() };
-        *arg = Operand::Move(place);
-    }
-}
-
 pub(super) enum DeadStoreElimination {
     Initial,
     Final,
@@ -146,7 +43,7 @@ impl<'tcx> crate::MirPass<'tcx> for DeadStoreElimination {
     }
 
     fn run_pass(&self, tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>) {
-        eliminate(tcx, body);
+        self.eliminate(tcx, body);
     }
 
     fn is_required(&self) -> bool {
@@ -154,25 +51,137 @@ impl<'tcx> crate::MirPass<'tcx> for DeadStoreElimination {
     }
 }
 
-/// Return the set of non-SSA locals that appear in debuginfo.
-fn debuginfo_non_ssa_locals<'tcx>(tcx: TyCtxt<'tcx>, body: &Body<'tcx>) -> DenseBitSet<Local> {
-    let typing_env = body.typing_env(tcx);
-    let ssa = SsaLocals::new(tcx, body, typing_env);
-    let mut non_ssa_locals: DenseBitSet<Local> = DenseBitSet::new_empty(body.local_decls.len());
-    for (local, _) in body.local_decls.iter_enumerated() {
-        if !ssa.is_ssa(local) {
-            non_ssa_locals.insert(local);
+impl DeadStoreElimination {
+    /// Performs the optimization on the body
+    ///
+    /// The `borrowed` set must be a `DenseBitSet` of all the locals that are ever borrowed in this
+    /// body. It can be generated via the [`borrowed_locals`] function.
+    fn eliminate<'tcx>(&self, tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>) {
+        let borrowed_locals = borrowed_locals(body);
+
+        // If the user requests complete debuginfo, mark the locals that appear in it as live, so
+        // we don't remove assignments to them.
+        let mut always_live = self.debuginfo_non_ssa_locals(tcx, body);
+        always_live.union(&borrowed_locals);
+
+        let mut live = MaybeTransitiveLiveLocals::new(&always_live)
+            .iterate_to_fixpoint(tcx, body, None)
+            .into_results_cursor(body);
+
+        // For blocks with a call terminator, if an argument copy can be turned into a move,
+        // record it as (block, argument index).
+        let mut call_operands_to_move = Vec::new();
+        let mut patch = Vec::new();
+
+        for (bb, bb_data) in traversal::preorder(body) {
+            if let TerminatorKind::Call { ref args, .. } = bb_data.terminator().kind {
+                let loc = Location { block: bb, statement_index: bb_data.statements.len() };
+
+                // Position ourselves between the evaluation of `args` and the write to `destination`.
+                live.seek_to_block_end(bb);
+                let mut state = live.get().clone();
+
+                for (index, arg) in args.iter().map(|a| &a.node).enumerate().rev() {
+                    if let Operand::Copy(place) = *arg
+                    && !place.is_indirect()
+                    // Do not skip the transformation if the local is in debuginfo, as we do
+                    // not really lose any information for this purpose.
+                    && !borrowed_locals.contains(place.local)
+                    && !state.contains(place.local)
+                    // If `place` is a projection of a disaligned field in a packed ADT,
+                    // the move may be codegened as a pointer to that field.
+                    // Using that disaligned pointer may trigger UB in the callee,
+                    // so do nothing.
+                    && is_within_packed(tcx, body, place).is_none()
+                    {
+                        call_operands_to_move.push((bb, index));
+                    }
+
+                    // Account that `arg` is read from, so we don't promote another argument to a move.
+                    LivenessTransferFunction(&mut state).visit_operand(arg, loc);
+                }
+            }
+
+            for (statement_index, statement) in bb_data.statements.iter().enumerate().rev() {
+                let loc = Location { block: bb, statement_index };
+                if let StatementKind::Assign(assign) = &statement.kind {
+                    if !assign.1.is_safe_to_remove() {
+                        continue;
+                    }
+                }
+                match &statement.kind {
+                    StatementKind::Assign(box (place, _))
+                    | StatementKind::SetDiscriminant { place: box place, .. }
+                    | StatementKind::Deinit(box place) => {
+                        if !place.is_indirect() && !always_live.contains(place.local) {
+                            live.seek_before_primary_effect(loc);
+                            if !live.get().contains(place.local) {
+                                patch.push(loc);
+                            }
+                        }
+                    }
+                    StatementKind::Retag(_, _)
+                    | StatementKind::StorageLive(_)
+                    | StatementKind::StorageDead(_)
+                    | StatementKind::Coverage(_)
+                    | StatementKind::Intrinsic(_)
+                    | StatementKind::ConstEvalCounter
+                    | StatementKind::PlaceMention(_)
+                    | StatementKind::BackwardIncompatibleDropHint { .. }
+                    | StatementKind::Nop => {}
+
+                    StatementKind::FakeRead(_) | StatementKind::AscribeUserType(_, _) => {
+                        bug!("{:?} not found in this MIR phase!", statement.kind)
+                    }
+                }
+            }
+        }
+
+        if patch.is_empty() && call_operands_to_move.is_empty() {
+            return;
+        }
+
+        let bbs = body.basic_blocks.as_mut_preserves_cfg();
+        for Location { block, statement_index } in patch {
+            bbs[block].statements[statement_index].make_nop();
+        }
+        for (block, argument_index) in call_operands_to_move {
+            let TerminatorKind::Call { ref mut args, .. } = bbs[block].terminator_mut().kind else {
+                bug!()
+            };
+            let arg = &mut args[argument_index].node;
+            let Operand::Copy(place) = *arg else { bug!() };
+            *arg = Operand::Move(place);
         }
     }
-    let mut ref_locals_visitor = RefLocals(non_ssa_locals);
-    ref_locals_visitor.visit_body(body);
-    let non_ssa_locals = ref_locals_visitor.0;
-    let mut visitor = DebuginfoLocals(DenseBitSet::new_empty(body.local_decls.len()));
-    for debuginfo in body.var_debug_info.iter() {
-        visitor.visit_var_debug_info(debuginfo);
+
+    /// Return the set of non-SSA locals that appear in debuginfo.
+    fn debuginfo_non_ssa_locals<'tcx>(
+        &self,
+        tcx: TyCtxt<'tcx>,
+        body: &Body<'tcx>,
+    ) -> DenseBitSet<Local> {
+        let mut visitor = DebuginfoLocals(DenseBitSet::new_empty(body.local_decls.len()));
+        for debuginfo in body.var_debug_info.iter() {
+            visitor.visit_var_debug_info(debuginfo);
+        }
+        if !matches!(self, DeadStoreElimination::Initial) {
+            let typing_env = body.typing_env(tcx);
+            let ssa = SsaLocals::new(tcx, body, typing_env);
+            let mut non_ssa_locals: DenseBitSet<Local> =
+                DenseBitSet::new_empty(body.local_decls.len());
+            for (local, _) in body.local_decls.iter_enumerated() {
+                if !ssa.is_ssa(local) {
+                    non_ssa_locals.insert(local);
+                }
+            }
+            let mut ref_locals_visitor = RefLocals(non_ssa_locals);
+            ref_locals_visitor.visit_body(body);
+            let non_ssa_locals = ref_locals_visitor.0;
+            visitor.0.intersect(&non_ssa_locals);
+        }
+        visitor.0
     }
-    visitor.0.intersect(&non_ssa_locals);
-    visitor.0
 }
 
 struct RefLocals(DenseBitSet<Local>);
@@ -181,11 +190,11 @@ impl Visitor<'_> for RefLocals {
     fn visit_assign(&mut self, place: &Place<'_>, rvalue: &Rvalue<'_>, _: Location) {
         // Even if we eliminate this statement, we can still calculate it in the debugging information,
         // so let's keep it around for now as a non-SSA local.
-        if let Some(local) = place.as_local()
-            && matches!(rvalue, Rvalue::Ref(..))
-        {
-            self.0.insert(local);
-        }
+        // if let Some(local) = place.as_local()
+        //     && matches!(rvalue, Rvalue::Ref(..))
+        // {
+        //     self.0.insert(local);
+        // }
     }
 }
 
