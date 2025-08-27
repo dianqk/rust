7edb592696d0f00b12a32d20ff20ca07506c3fc7
dianqk <dianqk@dianqk.net>
2025-08-27 14:41:52 +0800

    fixup! codegen: Generate `dbg_value` for the ref statement
    `&_1` is only valid if the pass mode is indirect.

--- compiler/rustc_codegen_ssa/src/mir/statement.rs
+++ compiler/rustc_codegen_ssa/src/mir/statement.rs
@@ -1,10 +1,10 @@
 use rustc_middle::mir::{self, NonDivergingIntrinsic, StmtDebugInfo};
 use rustc_middle::{bug, span_bug};
+use rustc_target::callconv::PassMode;
 use tracing::instrument;
 
 use super::{FunctionCx, LocalRef};
 use crate::common::TypeKind;
-use crate::mir::operand::OperandValue;
 use crate::mir::place::PlaceRef;
 use crate::traits::*;
 
@@ -110,24 +110,23 @@ impl<'a, 'tcx, Bx: BuilderMethods<'a, 'tcx>> FunctionCx<'a, 'tcx, Bx> {
         match debuginfo {
             StmtDebugInfo::AssignRef(dest, place) => {
                 let local_ref = match self.locals[place.local] {
-                    LocalRef::Place(place_ref) | LocalRef::UnsizedPlace(place_ref) => {
-                        Some(place_ref)
+                    LocalRef::Place(place_ref) | LocalRef::UnsizedPlace(place_ref) =>
+                        Some(place_ref),
+
+                    LocalRef::Operand(operand_ref) => {
+                        let pointer = operand_ref.val.pointer_parts().0;
+                        Some(PlaceRef::new_sized(pointer, operand_ref.layout))
                     }
-                    LocalRef::Operand(operand_ref) => match operand_ref.val {
-                        OperandValue::Immediate(v) => {
-                            Some(PlaceRef::new_sized(v, operand_ref.layout))
-                        }
-                        OperandValue::Ref(_)
-                        | OperandValue::Pair(_, _)
-                        | OperandValue::ZeroSized => None,
-                    },
                     LocalRef::PendingOperand => None,
                 }
                 .filter(|place_ref| {
+                    // For the reference of an argument (e.x. `&_1`), it's only valid if the pass mode is indirect, and its reference is
+                    // llval.
+                    matches!(place.as_local().and_then(|local| self.fn_abi.args.get(local.as_usize() - 1).map(|arg| &arg.mode)), Some(&PassMode::Indirect {..}) | None) &&
                     // Drop unsupported projections.
                     // FIXME: Add a test case.
                     place.projection.iter().all(|p| p.can_use_in_debuginfo()) &&
-                        // Only pointers can calculate addresses.
+                        // Only pointers can be calculated addresses.
                         bx.type_kind(bx.val_ty(place_ref.val.llval)) == TypeKind::Pointer
                 });
                 if let Some(local_ref) = local_ref {
