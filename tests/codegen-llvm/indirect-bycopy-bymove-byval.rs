2fe23c54e15b09388b80bee4f333e4a065493bb0
dianqk <dianqk@dianqk.net>
2026-04-16 21:21:14 +0800

    codegen: Copy to an alloca when the argument is neither by-val nor by-move for indirect pointer.

--- tests/codegen-llvm/indirect-bycopy-bymove-byval.rs
+++ tests/codegen-llvm/indirect-bycopy-bymove-byval.rs
@@ -1,3 +1,6 @@
+//! Regression test for issue <https://github.com/rust-lang/rust/issues/155241>.
+//! Arguments passed indirectly via a hidden pointer must be copied to an alloca,
+//! except for by-val or by-move.
 //@ compile-flags: -Cno-prepopulate-passes -Copt-level=3
 //@ only-x86_64-unknown-linux-gnu
 
@@ -7,6 +10,8 @@
 
 #[derive(Copy, Clone)]
 struct Thing(usize, usize, usize);
+
+// The argument of the second call is a by-move argument.
 
 // CHECK-LABEL: @normal
 // CHECK: call void @llvm.memcpy.p0.p0.i64(ptr align 8 [[normal_V1:%.*]], ptr align 8 %value, i64 24, i1 false)
@@ -28,15 +33,17 @@ pub fn normal() {
     opaque(VALUE);
 }
 
-// FIXME: closure#0 and closure#1 are missing memcpy.
+// The argument of the second call is a by-move argument.
 
 // CHECK-LABEL: @untupled
+// CHECK: call void @llvm.memcpy.p0.p0.i64(ptr align 8 [[untupled_V1:%.*]], ptr align 8 %value, i64 24, i1 false)
 // CHECK: call indirect_bycopy_bymove_byval::untupled::{closure#0}
-// CHECK-NEXT: call void @{{.*}}(ptr {{.*}}, ptr{{.*}} %value)
+// CHECK-NEXT: call void @{{.*}}(ptr {{.*}}, ptr{{.*}} [[untupled_V1]])
 // CHECK: call indirect_bycopy_bymove_byval::untupled::{closure#1}
 // CHECK-NEXT: call void @{{.*}}(ptr {{.*}}, ptr{{.*}} %value)
+// CHECK: call void @llvm.memcpy.p0.p0.i64(ptr align 8 [[untupled_V3:%.*]], ptr align 8 @anon{{.*}}, i64 24, i1 false)
 // CHECK: call indirect_bycopy_bymove_byval::untupled::{closure#2}
-// CHECK-NEXT: call void @{{.*}}(ptr {{.*}}, ptr{{.*}} @anon{{.*}})
+// CHECK-NEXT: call void @{{.*}}(ptr {{.*}}, ptr{{.*}} [[untupled_V3]])
 #[unsafe(no_mangle)]
 pub fn untupled() {
     let value = (Thing(0, 0, 0),);
@@ -58,14 +65,12 @@ pub fn untupled() {
     .call(VALUE);
 }
 
-// FIXME: all memcpy calls are redundant for byval.
+// All memcpy calls are redundant for byval.
 
 // CHECK-LABEL: @byval
-// CHECK: call void @llvm.memcpy.p0.p0.i64(ptr align 8 [[byval_V1:%.*]], ptr align 8 %value, i64 24, i1 false)
-// CHECK: call void @opaque_byval(ptr{{.*}} byval([24 x i8]){{.*}} [[byval_V1]])
 // CHECK: call void @opaque_byval(ptr{{.*}} byval([24 x i8]){{.*}} %value)
-// CHECK: call void @llvm.memcpy.p0.p0.i64(ptr align 8 [[byval_V3:%.*]], ptr align 8 @anon{{.*}}, i64 24, i1 false)
-// CHECK: call void @opaque_byval(ptr{{.*}} byval([24 x i8]){{.*}} [[byval_V3]])
+// CHECK: call void @opaque_byval(ptr{{.*}} byval([24 x i8]){{.*}} %value)
+// CHECK: call void @opaque_byval(ptr{{.*}} byval([24 x i8]){{.*}} @anon{{.*}})
 #[unsafe(no_mangle)]
 pub fn byval() {
     #[derive(Copy, Clone)]
